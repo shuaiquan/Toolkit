b35df86bf78c33860b040ab393151387b5a18e64
shuaiquan <994011557@qq.com>
2019-10-15 19:41:34 +0800

    throttle 添加isLeading 选择在 开始/结束 调用

--- src/common/Throttle/index.ts
+++ src/common/Throttle/index.ts
@@ -1,16 +1,20 @@
-export function throttle(func: Function, wait: number = 0) {
+export function throttle(func: Function, wait: number = 0, isLeading: boolean = false) {
     let lastInvokedTime: number = 0;
     return () => {
         const now = performance.now();
         if (lastInvokedTime === 0) {
             lastInvokedTime = now;
-            setTimeout(func, wait);
+            invokeImmediately(func, wait, isLeading);
         } else {
             const diffTime = now - lastInvokedTime;
             if (diffTime > wait) {
                 lastInvokedTime = now;
-                setTimeout(func, wait);
+                invokeImmediately(func, wait, isLeading);
             }
         }
     };
+}
+
+function invokeImmediately(func: Function, wait: number, isLeading: boolean) {
+    return isLeading ? func() : setTimeout(func, wait);
 }
