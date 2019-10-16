73713587f87857c4f02ce41ee088f19d5560f8a0
shuaiquan <994011557@qq.com>
2019-10-16 20:50:41 +0800

    debounce add options for leading and trailing

--- src/common/Debounce/index.ts
+++ src/common/Debounce/index.ts
@@ -1,18 +1,55 @@
-export function debounce(func: Function, wait: number = 0, isLeading: boolean = false) {
-    let timer: number;
-    return () => {
-        if (isLeading) {
-            if (timer) {
-                clearTimeout(timer);
-            } else {
-                func();
-            }
-            timer = setTimeout(() => timer = undefined, wait);
-        } else {
-            if (timer) {
-                clearTimeout(timer);
-            }
-            timer = setTimeout(func, wait);
+interface Options {
+    leading?: boolean;
+    trailing?: boolean;
+}
+
+const DEFUALT_OPTIONS = {
+    trailing: true,
+}
+
+export function debounce(func: Function, wait: number = 0, options: Options = DEFUALT_OPTIONS) {
+    let lastExcuteTime: number;
+    let lastInvokeTime: number;
+    let timerId: number;
+
+    const { leading, trailing } = options;
+
+    const excute = (time: number) => {
+        lastExcuteTime = time;
+        func();
+    }
+
+    const leadExcute = (time: number) => {
+        if (lastInvokeTime === undefined || time - lastInvokeTime > wait) {
+            excute(time);
         }
     }
+
+    const trailExcute = (time: number) => {
+        if (time === lastExcuteTime) {
+            return;
+        }
+        if (time - lastInvokeTime > wait && timerId) {
+            timerId = undefined;
+            excute(time);
+            return;
+        }
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            const nextTime = performance.now();
+            trailExcute(nextTime);
+        }, wait);
+    }
+
+    return () => {
+        const now = performance.now();
+        if (leading) {
+            leadExcute(now);
+        }
+        if (trailing) {
+            trailExcute(now);
+        }
+        lastInvokeTime = now;
+    };
 }
+
