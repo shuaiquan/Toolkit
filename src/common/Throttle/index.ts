6212d875df37a33df315f7c9cbd48d43c4c5366c
shuaiquan <994011557@qq.com>
2019-10-18 21:53:57 +0800

    参考 lodash.debounce maxing 改写 throttle && 添加注解

--- src/common/Throttle/index.ts
+++ src/common/Throttle/index.ts
@@ -1,20 +1,68 @@
-export function throttle(func: Function, wait: number = 0, isLeading: boolean = false) {
-    let lastInvokedTime: number = 0;
-    return () => {
-        const now = performance.now();
-        if (lastInvokedTime === 0) {
-            lastInvokedTime = now;
-            invokeImmediately(func, wait, isLeading);
-        } else {
-            const diffTime = now - lastInvokedTime;
-            if (diffTime > wait) {
-                lastInvokedTime = now;
-                invokeImmediately(func, wait, isLeading);
-            }
-        }
-    };
+interface Options {
+    leading?: boolean;
+    trailing?: boolean;
 }
 
-function invokeImmediately(func: Function, wait: number, isLeading: boolean) {
-    return isLeading ? func() : setTimeout(func, wait);
+const DEFUALT_OPTIONS = {
+    trailing: true,
+}
+
+
+export function throttle(func: Function, wait: number = 0, options: Options = DEFUALT_OPTIONS) {
+    // throttled function 被调用时间
+    let lastCallTime: number;
+    // func 实际的执行时间（默认为：0）
+    let lastInvokTime: number = 0;
+    // 检查 trailingEdge 是否执行的计时器
+    let timerId: number;
+
+    // 标记有 trailingEdge 函数待执行
+    let readyCallBack: boolean = false;
+
+    const { leading, trailing } = options;
+
+    const invokeFunc = (time: number) => {
+        lastInvokTime = time;
+        readyCallBack = false;
+        func();
+    }
+
+    const leadingEdge = (time: number) => {
+        if (!lastCallTime || time - lastCallTime > wait) {
+            invokeFunc(time);
+        }
+    }
+
+    const trailingEdge = (time: number) => {
+        timerId = undefined;
+        invokeFunc(time);
+    }
+
+    // 根据 lastInvokeTime 计算 trailingEdge 函数等待时间
+    const remainingWait = (time: number) => {
+        const timeSinceLastInvoke = time - lastInvokTime;
+        return wait - timeSinceLastInvoke;
+    }
+
+    const timeExpired = () => {
+        const currentTime = performance.now();
+        if (readyCallBack && (currentTime - lastInvokTime) > wait) {
+            trailingEdge(currentTime);
+        }
+        if (!timerId) {
+            timerId = setTimeout(timeExpired, remainingWait(currentTime));
+        }
+    }
+
+    return () => {
+        const currentTime = performance.now();
+        readyCallBack = true;
+        if (leading) {
+            leadingEdge(currentTime);
+        }
+        if (trailing) {
+            timerId = setTimeout(timeExpired, wait);
+        }
+        lastCallTime = currentTime;
+    };
 }
