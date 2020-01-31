dd272163a1ae1e9e9fe95e667fe795e266bbdfbb
shuaiquan <994011557@qq.com>
2020-01-31 19:56:59 +0800

    完善 Container 的实现

--- src/Data-Flow/Flux/Container.ts
+++ src/Data-Flow/Flux/Container.ts
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReduceStore from './ReduceStore';
 import { shallowEqualObjects } from 'shallow-equal';
+import ListenToStores from './ListenToStores';
 
 interface ReactClass<Props, State> {
     new(props: Props, context: any): React.Component<Props, State>;
 }
 
 type ContainerBaseClass<Props, State> = {
-    getStores: () => ReduceStore<any>[];
-    calculateState: () => State;
+    getStores: (props?: Props, context?: any) => ReduceStore<any>[];
+    calculateState: (preState: State, props?: Props, context?: any) => State;
 } & ReactClass<Props, State>;
 
 interface Options {
@@ -21,26 +22,65 @@ interface Options {
  * 创建 Container 组件
  * @param Base 
  */
-function create<Props, State>(Base: ContainerBaseClass<Props, State>, options: Options) {
+function create<Props, State>(Base: ContainerBaseClass<Props, State>, options: Options = { pure: true }) {
     enforceInterface(Base);
 
+    /**
+     * 根据用户配置设置 Stores
+     * @param maybeProps 
+     * @param maybeContext 
+     */
+    const getStores = (maybeProps: Props, maybeContext: any) => {
+        const props = options.withProps ? maybeProps : undefined;
+        const context = options.withContext ? maybeContext : undefined;
+        return Base.getStores(props, context);
+    };
+
+    /**
+     * 根据用户配置重新计算 state
+     * @param state 
+     * @param maybeProps 
+     * @param maybeContext 
+     */
+    const getState = (state: State, maybeProps: Props, maybeContext: any) => {
+        const props = options.withProps ? maybeProps : undefined;
+        const context = options.withContext ? maybeContext : undefined;
+        return Base.calculateState(state, props, context);
+    }
+
     class Com extends Base {
+        private listenToStores: ListenToStores = null;
+
         constructor(props: Props, context: any) {
             super(props, context);
-            this.listenToStore();
-            this.state = Base.calculateState();
+            const stores = getStores(props, context);
+            // 监听 stores , 并注册更新函数
+            this.listenToStores = new ListenToStores(stores, this.updateState);
+            // 初始化 state
+            this.updateState();
         }
 
-        private listenToStore() {
-            const stores = Base.getStores();
-            stores.forEach(store => {
-                store.addEventListener(this.updateState);
-            });
+        UNSAFE_componentWillReceiveProps(nextProps: Props, nextContext: any) {
+            if (super.componentWillReceiveProps) {
+                super.componentWillReceiveProps(nextProps, nextContext);
+            }
+            if (options.withProps || options.withContext) {
+                const stores = getStores(nextProps, nextContext);
+                this.listenToStores.setStores(stores);
+                this.setState(prevState => getState(prevState, nextProps, nextContext));
+            }
+        }
+
+
+        componentWillUnmount() {
+            if (super.componentWillUnmount) {
+                super.componentWillUnmount();
+            }
+            this.listenToStores.dispose();
         }
 
         private updateState = () => {
-            const state = Base.calculateState();
-            this.setState(state);
+            this.setState(prevState => getState(prevState, this.props, this.context));
         }
     }
 
