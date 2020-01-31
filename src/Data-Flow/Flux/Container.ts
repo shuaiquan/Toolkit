a2f6a27c4a539d6c2b0ea51322b46e7bc76232f3
shuaiquan <994011557@qq.com>
2020-01-31 10:33:01 +0800

    创建基本的 Container 结构。待完善

--- src/Data-Flow/Flux/Container.ts
+++ src/Data-Flow/Flux/Container.ts
@@ -1,19 +1,76 @@
 import React from 'react';
 import ReduceStore from './ReduceStore';
+import shallowequal from 'shallowequal';
 
-export default abstract class Container<P, S, StoreState> extends React.Component<P, S> {
-    abstract getStores(): ReduceStore<any>[];
+interface ReactClass<Props, State> {
+    new(props: Props, context: any): React.Component<Props, State>;
+}
 
-    abstract calculateState(): StoreState;
+type ContainerBaseClass<Props, State> = {
+    getStores: () => ReduceStore<any>[];
+    calculateState: () => State;
+} & ReactClass<Props, State>;
 
-    constructor(props: P, context: any) {
-        super(props, context);
+interface Options {
+    pure?: boolean,
+    withProps?: boolean,
+    withContext?: boolean,
+};
 
-        this.getStores().forEach(store => store.addEventListener(this.subscribeStore));
+/**
+ * 创建 Container 组件
+ * @param Base 
+ */
+function create<Props, State>(Base: ContainerBaseClass<Props, State>, options: Options) {
+    enforceInterface(Base);
+
+    class Com extends Base {
+        constructor(props: Props, context: any) {
+            super(props, context);
+            this.listenToStore();
+            this.state = Base.calculateState();
+        }
+
+        private listenToStore() {
+            const stores = Base.getStores();
+            stores.forEach(store => {
+                store.addEventListener(this.updateState);
+            });
+        }
+
+        private updateState = () => {
+            const state = Base.calculateState();
+            this.setState(state);
+        }
     }
 
-    private subscribeStore = () => {
-        const state = this.calculateState();
-        this.setState(state);
+    return options.pure ? createPureComponent(Com) : Com;
+}
+
+/**
+ * 如果用户选择了 pure 组件，默认添加 shallowEqual 的比较
+ * @param Container 
+ */
+function createPureComponent<Props, State>(Container: ReactClass<Props, State>) {
+    return class extends Container {
+        shouldComponentUpdate(nextProps: Props, nextState: State) {
+            return !shallowequal(this.props, nextProps) || !shallowequal(this.state, nextState);
+        }
     }
 }
+
+/**
+ * 检测 Base 组件是否实现了 getStores 和 calculateState 方法
+ * @param base 
+ */
+function enforceInterface(base: any) {
+    if (!base.getStores) {
+        throw new Error('Components that use FluxContainer must implement `static getStores()`');
+    }
+
+    if (!base.calculateState) {
+        throw new Error('Components that use FluxContainer must implement `static calculateState()`');
+    }
+}
+
+export default { create };
