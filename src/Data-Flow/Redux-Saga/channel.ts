bb90b7597939a781a6b2ab24ae8a5144dc91ae5c
shuaiquan <994011557@qq.com>
2020-03-11 22:57:54 +0800

    明确最小版本的 Taker Channel Matcher 数据结构及功能

--- src/Data-Flow/Redux-Saga/channel.ts
+++ src/Data-Flow/Redux-Saga/channel.ts
@@ -1,21 +1,40 @@
-import { Matcher } from './matcher';
-import { remove } from './util';
+import { Matcher, wildcard } from './matcher';
+import { AnyAction } from '../Redux/types';
 
+/**
+ * 最简单的版本定义 Taker 
+ * 
+ * cb: 一个监听函数
+ * matcher: 一个匹配Action的方法
+ */
 interface Taker {
-    cb: Function,
+    cb: TakerFunc,
     matcher: Matcher;
-    cancel: () => void;
+}
+
+interface TakerFunc {
+    (action: AnyAction): void;
 }
 
 class MultiCastChannel {
     private takers: Taker[] = [];
 
-    take(cb: Function, matcher: Matcher) {
-
+    /**
+     * 将监听函数添加到队列里
+     */
+    take(cb: TakerFunc, matcher: Matcher = wildcard) {
+        this.takers.push({ cb, matcher });
     }
 
-    put(input) {
-
+    /**
+     * 找到匹配当前 Action 的监听函数并执行
+     */
+    put(action: AnyAction) {
+        this.takers.forEach(taker => {
+            if (taker.matcher(action)) {
+                taker.cb(action);
+            }
+        })
     }
 }
 
--- src/Data-Flow/Redux-Saga/matcher.ts
+++ src/Data-Flow/Redux-Saga/matcher.ts
@@ -1,3 +1,20 @@
+import { AnyAction } from "../Redux/types";
+
 export interface Matcher {
-    (param: any): boolean;
+    (action: AnyAction): boolean;
+}
+
+export const wildcard = () => true;
+export const string = (pattern: string) => (action: AnyAction) => action.type === pattern;
+
+/**
+ * 最简单的版本只支持字符串类型的匹配
+ */
+export function matcherFactory(pattern: string) {
+    switch (pattern) {
+        case '*':
+            return wildcard;
+        default:
+            return string;
+    }
 }
