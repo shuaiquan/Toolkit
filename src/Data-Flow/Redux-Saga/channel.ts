1b89302bd56cced2ce47620700fa47a2ee910101
shuaiquan <994011557@qq.com>
2020-03-15 14:59:45 +0800

    channel 补充 close 接口

--- src/Data-Flow/Redux-Saga/channel.ts
+++ src/Data-Flow/Redux-Saga/channel.ts
@@ -16,13 +16,25 @@ interface TakerFunc {
     (action: AnyAction): void;
 }
 
+const END_TYPE = 'CLOSE_CHANNEL';
+export const END = { type: END_TYPE };
+export function isEnd(action: AnyAction) {
+    return action.type === END_TYPE;
+}
+
 class MultiCastChannel {
     private takers: Taker[] = [];
+
+    private closed: boolean = false;
 
     /**
      * 将监听函数添加到队列里
      */
     take(cb: TakerFunc, matcher: Matcher = wildcard) {
+        if (this.closed) {
+            cb(END);
+        }
+
         this.takers.push({ cb, matcher });
     }
 
@@ -30,11 +42,28 @@ class MultiCastChannel {
      * 找到匹配当前 Action 的监听函数并执行
      */
     put(action: AnyAction) {
+        if (this.closed) {
+            return;
+        }
+
+        if (isEnd(action)) {
+            this.close();
+        }
+
         this.takers.forEach(taker => {
             if (taker.matcher(action)) {
                 taker.cb(action);
             }
         })
+    }
+
+    /**
+     * 关闭了当前的 Channel
+     */
+    close() {
+        this.closed = true;
+
+        this.takers.forEach(taker => taker.cb(END));
     }
 }
 
